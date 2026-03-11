9e2c1eb3cd7d1c45ea415ec4486a94e940e6e576
Harsh Kumar <yadavharsh1201@gmail.com>
2026-03-11 16:46:27 +0530

    add email notification on successful Cloudinary media upload

--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import nodemailer from "nodemailer";
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -15,5 +16,34 @@ const fileSchema = new mongoose.Schema({
     type: String,
   },
 });
+
+// post middleware
+fileSchema.post("save", async function(doc){
+  try{
+    console.log("Doc:", doc);
+
+    // transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      },
+    });
+
+    // send mail
+    let info = await transporter.sendMail({
+      from: "JohnWick",
+      to: doc.email,
+      subject: "New File Uploaded on Cloudinary",
+      html: `<h2>Hello Jee</h2> <p>File is successfully uploaded on cloudinary. View Here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
+    });
+
+    console.log("INFO -> ", info);
+
+  } catch(error){
+    console.error(error);
+  }
+})
 
 export const File = mongoose.model("File", fileSchema);
